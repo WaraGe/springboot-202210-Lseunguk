a56503ee970a902bf03a381048692877b146e74a
warage <zpfhfh774@naver.com>
2022-12-07 22:01:06 +0900

    Spring#1

---
+++ src/main/java/com/study/springboot202210Lseunguk/Springboot202210LseungukApplication.java
@@ -0,0 +1,13 @@
+package com.study.springboot202210Lseunguk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot202210LseungukApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot202210LseungukApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/study/springboot202210Lseunguk/web/controller/Controller1.java
@@ -0,0 +1,23 @@
+package com.study.springboot202210Lseunguk.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class Controller1 {
+
+//  사진의 Controller클래스
+//    @RequestMapping(value = "/page1", method = RequestMethod.GET);
+    @GetMapping("/page1")
+    public String page1() {
+        return "page1";
+//      html 파일의 이름만 return 해주면 됨
+    }
+    @GetMapping("/page2")
+    public String page2() {
+        return "page2";
+//      html 파일의 이름만 return 해주면 됨
+    }
+}
